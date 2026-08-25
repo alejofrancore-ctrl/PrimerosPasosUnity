f53e9d4594210fce7aaaf5aa8d579ebd25e6b970
alejofrancore-ctrl <293688172+alejofrancore-ctrl@users.noreply.github.com>
2026-08-25 11:24:03 -0500

    taller sintaxis

---
+++ Myunity/Assets/_main/scripts/ObstaculoMortal.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ObstaculoMortal : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
