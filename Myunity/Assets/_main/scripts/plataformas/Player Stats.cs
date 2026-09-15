b75d5f61e655bd66c8cc9cf3d0724be0e62716fe
alejofrancore-ctrl <293688172+alejofrancore-ctrl@users.noreply.github.com>
2026-09-15 11:40:58 -0500

    vida maximay muerte por puntos

--- Myunity/Assets/_main/scripts/plataformas/Player Stats.cs
+++ Myunity/Assets/_main/scripts/plataformas/Player Stats.cs
@@ -3,33 +3,43 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] private float _puntosVida = 100.0f;
+    [SerializeField] private float _puntosVidaactuales = 100.0f;
     [SerializeField] private UImanager _uimanager;
     public void RestarVida(int daño)
     {
-        _puntosVida= _puntosVida-daño;
+        _puntosVidaactuales= _puntosVidaactuales-daño;
         
 
        
     }
     public void SumarVida(int salud)
     {
-        _puntosVida= _puntosVida+salud;
+        _puntosVidaactuales= _puntosVidaactuales+salud;
         
     }
     private void Update()
     {
-        if (_puntosVida>=70.0f)
+        if (_puntosVidaactuales>=70.0f)
         {
             _uimanager.ColorBarra(Color.green);
         }
-        if (_puntosVida>40 && _puntosVida<=70)
+        if (_puntosVidaactuales>40 && _puntosVidaactuales<=70)
         {
             _uimanager.ColorBarra(new Color(1f, 0.64f, 0f,1f));
         }
-            if (_puntosVida<=30.0f)
+            if (_puntosVidaactuales<=30.0f)
         {
             _uimanager.ColorBarra(Color.red);
+        }
+        //igualar vida a 100
+        if (_puntosVidaactuales > 100)
+        {
+            _puntosVidaactuales=100;
+        }
+        //vida 0 = muerte
+       if (_puntosVidaactuales <= 0)
+        {
+            Destroy(this.gameObject);
         }
         }
     }
